b61b556119dc69f7f42055ad5ca44f480a768414
Waqas Ahmed <wa02263@gmail.com>
2023-09-30 01:12:18 -0700

    one more

--- string/problem_67.dart
+++ string/problem_67.dart
@@ -1,91 +1,31 @@
-import 'dart:ffi';
-
 class Solution {
   String addBinary(String a, String b) {
     if (b.isEmpty) return a;
     String output = "";
     bool isCarry = false;
-// for (Char aBinary in a){
+    int maxLength = a.length > b.length ? a.length : b.length;
+    a = a.padLeft(maxLength, '0');
+    b = b.padLeft(maxLength, '0');
 
-// }
+    for (int i = maxLength - 1; i >= 0; i--) {
+      int digitA = int.parse(a[i]);
+      int digitB = int.parse(b[i]);
 
-    for (int i = a.length - 1; i >= 0; i--) {
-      if (a[i] == "0" && b[i] == "0") {
-        if (isCarry == false) {
-          output += "0";
-          print("output at 1 if $output and carry is false");
-        } else {
-          if (i != 0) {
-            output += "1";
-            print("output at 1 if $output and carry is true");
-
-            isCarry = false;
-          } else {
-            output += "10";
-            // print("output at 1 if $output and carry is true");
-            print("executed at 1 if");
-
-
-            isCarry = false;
-          }
-        }
-      } else if (a[i] == "1" && b[i] == "1") {
-        if (isCarry == false) {
-          output += "0";
-          print("output at 2 if $output and carry is false");
-          isCarry = true;
-        } else {
-          if (i != 0) {
-            output += "1";
-            print("output at 3 if $output and carry is true");
-
-            isCarry = true;
-          } else {
-            output += "11";
-            // print("output at 3 if $output and carry is true");
-            print("executed at 2 if");
-
-          }
-        }
-      } else if ((a[i] == "1" && b[i] == "0") || (a[i] == "0" && b[i] == "1")) {
-        if (isCarry == false) {
-          output += "1";
-          print("output at 3 if $output and carry is false");
-        } else {
-          if (i != 0) {
-            output += "0";
-            print("output at 3 if $output and carry is true");
-
-            isCarry = false;
-          } else {
-            output += "01";
-            print("executed at 3 if");
-            // print("output at 3 if $output and carry is true");
-          }
-        }
+      int sum = digitA + digitB + (isCarry ? 1 : 0);
+      if (sum >= 2) {
+        isCarry = true;
+        sum -= 2;
+      } else {
+        isCarry = false;
       }
-      //  else if (a[i] == "0" && b[i] == "1") {
-      //   if (isCarry == false) {
-      //     output += "1";
-      //   } else {
-      //     output += "1";
-      //   }
-      // }
-      // isCarry = false;
 
-      // if (a[i] + b[i] == 2) {
-      //   output += "0";
-      //   if (a[i - 1] == 1 && b[i - 1] == 1 && i != 0) {
-      //     output += '1';
-      //     a[i - 1].replaceRange(i - 1, i - 1, a[i - 1] + 1.toString());
-      //   } else if (a[i - 1] == 0 && b[i - 1] == 0 && i != 0) {
-      //     output += '1';
-      //   } else if (a[i - 1] == 0 && b[i - 1] == 1 && i != 0) {
-      //     output += '1';
-      //   }
-      // }
+      output = '$sum$output';
     }
-    print(output);
+
+    if (isCarry) {
+      output = '1$output';
+    }
+
     return output;
   }
 }
@@ -93,4 +33,5 @@ class Solution {
 void main() {
   Solution solution = Solution();
   String result = solution.addBinary("1010", "1011");
+  print(result); // Output: "100"
 }
