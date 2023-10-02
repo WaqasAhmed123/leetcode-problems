ab03bf812095e06b33d611ade00b03ef0f9809d7
Waqas Ahmed <wa02263@gmail.com>
2023-10-02 10:49:53 -0700

    one more

--- string/problem_290.dart
+++ string/problem_290.dart
@@ -2,18 +2,31 @@ class Solution {
   bool wordPattern(String pattern, String s) {
     List<String> individualWords = s.split(" ");
     Map<String, String> assignValues = {};
+    if (pattern.length != individualWords.length) return false;
+    for (int i = 0; i < pattern.length; i++) {
+      if (!assignValues.containsKey(pattern[i]) &&
+          !assignValues.containsValue(individualWords[i])) {
+        assignValues[pattern[i]] = individualWords[i];
+      }
+    }
     for (int i = 0; i < pattern.length; i++) {
       if (!assignValues.containsKey(pattern[i])) {
-        assignValues[pattern[i]] = individualWords[i];
+        print("false at first if");
+        return false;
+      } else if (assignValues[pattern[i]] != individualWords[i]) {
+        print("false at last if");
+
+        return false;
       }
     }
 
     print(assignValues);
+    print(true);
     return true;
   }
 }
 
 void main() {
   Solution solution = Solution();
-  bool result = solution.wordPattern("abba", "dog cat cat dog");
+  bool result = solution.wordPattern("abba", "dog dog dog dog");
 }
