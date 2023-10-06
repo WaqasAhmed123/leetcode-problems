6d96c24067d97909f557b4616719af367761eb90
Waqas Ahmed <wa02263@gmail.com>
2023-10-06 11:29:45 -0700

    one more

--- string/problem_345.dart
+++ string/problem_345.dart
@@ -1,33 +1,60 @@
 class Solution {
   String reverseVowels(String s) {
+    Map<String, String> vowels = {
+      "a": "A",
+      "e": "E",
+      "i": "I",
+      "o": "O",
+      "u": 'U'
+    };
+    
     List<String> sList = s.split("");
-    // int i = 0;
-    // int i = sList.length - 1;
-    int i = 0;
-    while (i < sList.length - 1) {
-      if (sList[i] == "a" ||
-          sList[i] == "e" ||
-          sList[i] == "i" ||
-          sList[i] == "o" ||
-          sList[i] == "u") {
-        String temp = sList[i];
-        sList[i] = sList[i];
-        sList[i] = temp;
-        // i++;
-        // i--;
-        i++;
-      } else {
-        // i++;
-        // i--;
-        i++;
+    int left = 0;
+    int right = sList.length - 1;
+    bool isFoundOnLeft = false;
+    bool isFoundOnRight = false;
+    late int saveRightIndex;
+    late int saveLeftIndex;
+    while (left < right) {
+      if (vowels.containsKey(sList[left]) ||
+          vowels.containsValue(sList[left])) {
+        print("left index is $left");
+        saveLeftIndex = left;
+        isFoundOnLeft = true;
       }
+      else{
+        left++;
+        continue;
+      }
+      if (vowels.containsKey(sList[right]) ||
+          vowels.containsValue(sList[right])) {
+        print("right index is $right");
+        saveRightIndex = right;
+        isFoundOnRight = true;
+      }
+      else{
+        right--;
+        continue;
+      }
+      if (isFoundOnRight && isFoundOnLeft) {
+        String temp = sList[saveLeftIndex];
+        sList[saveLeftIndex] = sList[saveRightIndex];
+        sList[saveRightIndex] = temp;
+        isFoundOnLeft = false;
+        isFoundOnRight = false;
+        print("executed");
+      }
+
+      left++;
+      right--;
     }
-    print(sList);
-    return sList.toString();
+    print(sList.join());
+    return sList.join();
   }
 }
 
 void main() {
   Solution solution = Solution();
-  var result = solution.reverseVowels("hello");
+  // var result = solution.reverseVowels("leetcode");
+  var result = solution.reverseVowels("Euston saw I was not Sue.");
 }
