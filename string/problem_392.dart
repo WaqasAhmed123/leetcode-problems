6b2a697f5d63a5a9f1a3fba808957127d323ca72
Waqas Ahmed <wa02263@gmail.com>
2023-10-15 11:01:17 -0700

    one more

--- string/problem_392.dart
+++ string/problem_392.dart
@@ -1,39 +1,17 @@
-
 class Solution {
   bool isSubsequence(String s, String t) {
-    // List<String> tList = t.split("");
-    // List<String> sList = s.split("");
-    // if (t.contains(s)) return true;
-    int countT = 0;
-    int countS = 0;
-    for (int i = 0; i < s.length; i++) {
-      // if (i < t.length - 1) {
-        if (t.codeUnits.contains(s.codeUnitAt(i))){
-        countS += s.codeUnitAt(i);
-
+    if (s.isEmpty) return true;
+    int matchedChar = 0;
+    for (int i = 0; i < t.length; i++) {
+      if (t[i] == s[matchedChar]) {
+        matchedChar++;
+        if (matchedChar == s.length) {
+          return true;
         }
-      countT += t.codeUnitAt(i);
       }
-    
+    }
 
-    // for (int i = 0; i < t.length; i++) {
-    //   // if (!s.codeUnits.contains(t.codeUnitAt(i))) {
-    //   if (!s.contains(t[i])) {
-    //     tList.remove(t[i]);
-    //   } else if (s.isNotEmpty && sList.contains(t[i])) {
-    //     print("executed");
-    //     sList.remove(t[i]);
-    //     tList.remove(t[i]);
-    //   }-
-    // }
-    var finalString = String.fromCharCode(t.codeUnits-s.codeUnits);
-    
-    print(finalString);
-
-    // print(tList);
-    // print(sList);
-    // print(tList.join() == s);
-    return tList.join() == s;
+    return false;
   }
 }
 
