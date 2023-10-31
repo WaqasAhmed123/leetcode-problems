dbe73e80a72ce2524a122237f58e30ccacf65827
Waqas Ahmed <wa02263@gmail.com>
2023-10-31 07:08:05 -0700

    one more solved

--- string/problem_459.dart
+++ string/problem_459.dart
@@ -1,14 +1,17 @@
 class Solution {
   bool repeatedSubstringPattern(String s) {
     for (int i = 0; i <= s.length; i++) {
+      // print(s.substring(0, i + 1));
       if (!s.substring(i).contains(s.substring(0, i))) {
-        // print(s.substring(i));
-        print(i);
-        print(s.substring(i).contains(s.substring(0, i - 1)));
-        return (s.substring(i).contains(s.substring(0, i - 1)));
+        print(s.substring(0, i - 1));
+        print(s.substring(i - 1));
+        // print(i);
+        print("-----");
+        print(s.substring(i - 1).contains(s.substring(0, i - 1)));
+        return (s.substring(i - 1).contains(s.substring(0, i - 1)));
       }
     }
-    print(false);
+    // print(false);
     return false;
   }
 }
@@ -16,5 +19,5 @@ class Solution {
 void main() {
   Solution solution = Solution();
   // int result = solution.countSegments("Hello, my name is John");
-  bool result = solution.repeatedSubstringPattern("abab");
+  bool result = solution.repeatedSubstringPattern("aba");
 }
