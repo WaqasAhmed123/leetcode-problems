2d00ca625e2a5fb93eee5d9cac7e4c6fee09119f
Waqas Ahmed <wa02263@gmail.com>
2023-11-04 00:52:02 -0700

    one more solved

---
+++ string/problem_520.dart
@@ -0,0 +1,14 @@
+class Solution {
+  bool detectCapitalUse(String word) {
+
+    return word == word.toUpperCase() ||
+        word == word.toLowerCase() ||
+        (word[0] == word[0].toUpperCase() &&
+            word.substring(1) == word.substring(1).toLowerCase());
+  }
+}
+
+void main() {
+  Solution solution = Solution();
+  bool result = solution.detectCapitalUse("flaG");
+}
