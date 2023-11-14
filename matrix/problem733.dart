1301bfe220e28da4f4d29762a9679ea5941ac4a9
Waqas Ahmed <wa02263@gmail.com>
2023-11-14 05:59:46 -0800

    one more solved

--- matrix/problem733.dart
+++ matrix/problem733.dart
@@ -1,31 +1,14 @@
 class Solution {
-  List<List<int>> matrixReshape(List<List<int>> mat, int r, int c) {
-    if (mat.length *mat[0].length != r * c) {
-    // Reshape is not possible, return the original matrix
-    return mat;
-  }
-
-    List<List<int>> newMatrix = List.generate(r, (_) => []);
-    int listAdder = 0;
-    for (int i = 0; i < mat.length; i++) {
-      for (int j = 0; j < mat[0].length; j++) {
-        newMatrix[listAdder].add(mat[i][j]);
-        if (newMatrix[listAdder].length == c) {
-          listAdder++;
-        }
-      }
-    }
-
+  List<List<int>> floodFill(List<List<int>> image, int sr, int sc, int color) {
     // newMatrix.add([] * r);
-    print(newMatrix);
-    return newMatrix;
+    return image;
   }
 }
 
 void main() {
   Solution solution = Solution();
-  List<List<int>> result = solution.matrixReshape([
-    [1, 2],
-    [3, 4]
-  ], 4, 1);
+  List<List<int>> result = solution.floodFill([
+    [0, 0, 0],
+    [0, 0, 0]
+  ], 0, 0, 0);
 }
