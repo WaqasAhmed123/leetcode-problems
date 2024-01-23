9fac933ac30d2562435abe242de18b2b666b30b9
Waqas Ahmed <wa02263@gmail.com>
2024-01-23 09:43:19 -0800

    one more

--- matrix/problem_766.dart
+++ matrix/problem_766.dart
@@ -1,18 +1,15 @@
 class Solution {
   bool isToeplitzMatrix(List<List<int>> matrix) {
-    for (int i = matrix.length; i > 0; i--)
-      if (matrix[i][i] != matrix[i - 1][i - 1]) {
+    for (int i =0; i <matrix.length-1; i++){
+      for(int j=0;j<matrix[i].length-1;j++){
+      if (matrix[i][j] != matrix[i + 1][j+1]) {
         print(false);
         return false;
       }
-    // int commonElement = matrix[0][0];
-    // for (int i = 0; i < matrix.length; i++) {
-    //   if(i<matrix[i].length)
-    //   if (matrix[i][i] != commonElement) {
-    //     print(false);
-    //     return false;
-    //   }
-    // }
+
+      }
+
+    }
     print(true);
     return true;
   }
@@ -21,8 +18,10 @@ class Solution {
 void main() {
   Solution solution = Solution();
   bool result = solution.isToeplitzMatrix([
-    [18], [66] // [1, 2, 3, 4],
+    // [18], [66] 
+    // [1, 2, 3, 4],
     // [5, 1, 2, 3],
     // [9, 5, 1, 2]
+    [1,2],[2,2]
   ]);
 }
