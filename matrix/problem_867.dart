2a510f986b087ac9a9916fa85b7b4d3b785c4777
Waqas Ahmed <wa02263@gmail.com>
2024-01-31 10:02:25 -0800

    one more

---
+++ matrix/problem_867.dart
@@ -0,0 +1,27 @@
+class Solution {
+  List<List<int>> transpose(List<List<int>> matrix) {
+    List<List<int>> transposedMatrix = [];
+    for (int i = 0; i < matrix.length; i++) {
+      List<int> row = [];
+      for (int j = 0; j < matrix[0].length; j++) {
+        // if (i <=j) {
+          row.add(matrix[j][i]);
+        // }
+      }
+      transposedMatrix.add(row);
+      print(row);
+    }
+    print(transposedMatrix);
+    return transposedMatrix;
+  }
+}
+
+void main() {
+  Solution solution = Solution();
+  List<List<int>> result = solution.transpose([
+    // [1, 2, 3],
+    // [4, 5, 6],
+    // [7, 8, 9]
+    [1, 2, 3], [4, 5, 6]
+  ]);
+}
