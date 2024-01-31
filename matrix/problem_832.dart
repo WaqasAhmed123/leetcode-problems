a6864683d5660ff8d083466339f1bad258fd1310
Waqas Ahmed <wa02263@gmail.com>
2024-01-31 09:18:44 -0800

    one more

--- matrix/problem_832.dart
+++ matrix/problem_832.dart
@@ -1,26 +1,19 @@
 class Solution {
   List<List<int>> flipAndInvertImage(List<List<int>> image) {
-    // print(image[image.length - 1]);
     for (int i = 0; i < image.length; i++) {
       for (int j = 0; j < (image[0].length ~/ 2); j++) {
-        // if (matrix[i][j] != matrix[i + 1][j + 1])
-        //  {
-        //   print(false);
-        //   return false;
-        // }
         print(image[i][j]);
         int tempt = image[i][j];
         image[i][j] = image[i][image[0].length - j - 1];
         image[i][image[0].length - j - 1] = tempt;
-        //  image[i][image[0].length - j + 1]= tempt;
       }
     }
-    print(image);
-    return [
-      [1, 1, 0],
-      [1, 0, 1],
-      [0, 0, 0]
-    ];
+    for (int i = 0; i < image.length; i++) {
+      for (int j = 0; j < (image[0].length); j++) {
+        image[i][j] == 0 ? image[i][j] = 1 : image[i][j] = 0;
+      }
+    }
+    return image;
   }
 }
 
